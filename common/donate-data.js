f78abcc964af6c0deb40203364f81e6e5699416b
ChenJianwei <cjw0511@qq.com>
2013-08-09 10:41:58 +0800

    更新 index.html 部分内容，添加 "关于信息" 等说明

--- common/donate-data.js
+++ common/donate-data.js
@@ -4,7 +4,8 @@
     $.util.namespace("donate");
 
     window.donate.data = [
-        { name: "*溪林 dua***@sina.com", date: "2013.08.08 16:55:42", total: 100 }
+        { name: "*溪林 dua***@sina.com", date: "2013.08.08 16:55:42", total: 100 },
+        { name: "*业 lin***@sina.com", date: "2013.08.08 16:48:35", total: 1 }
     ];
 
 })(jQuery);
